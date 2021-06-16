3aefbda72644061acf26edab812f78c2ce99d871
Rajat Gundi <glrajat@gmail.com>
2021-06-16 11:38:38 +0530

    Added: kafka-producer

---
+++ src/main/java/com/org/kafkaproducer/SpringBootKafkaProducerExample.java
@@ -0,0 +1,12 @@
+package com.org.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerExample {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootKafkaProducerExample.class,args);
+    }
+}
---
+++ src/main/java/com/org/kafkaproducer/publisher/UserResource.java
@@ -0,0 +1,26 @@
+package com.org.kafkaproducer.publisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+    private static final String TOPIC = "test";
+
+    @GetMapping("/publish/{message}")
+    public String post(@PathVariable("message") final String message){
+
+        kafkaTemplate.send(TOPIC,message);
+
+        return "Published Successfully";
+    }
+
+}
